29da24f83fa4254d25043b7367daebf974933c33
Remy Wang <remy.wang727@gmail.com>
2024-09-18 19:38:44 +0200

    Update install.sh

--- MongoDB/install.sh
+++ MongoDB/install.sh
@@ -4,3 +4,12 @@
 
 brew tap mongodb/brew
 brew install mongodb-community
+
+### Enable Access Control
+```bash
+sudo nano /usr/local/etc/mongod.conf
+
+security:
+  authorization: "enabled"
+```
+Restart
